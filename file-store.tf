4801b5124df4be8d2345f2c3412bad22516d27dc
Sivaram Kannan <sivaramsk@gmail.com>
2020-07-28 18:28:04 +0530

    Remove the storage account creation and use the storage account created by the azure admin.

--- file-store.tf
+++ file-store.tf
@@ -1,14 +1,6 @@
-resource "azurerm_storage_account" "default" {
-  name                = "intaindevstorageaccount"
-  location            = azurerm_resource_group.default.location
-  resource_group_name = azurerm_resource_group.default.name
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
 
 resource "azurerm_storage_share" "default" {
-  name                 = "akssharedstore"
-  storage_account_name = azurerm_storage_account.default.name
-  resource_group_name = azurerm_resource_group.default.name
+  name                 = var.file_share_name
+  storage_account_name = var.storage_account
   quota                = 50
 }
