f9f4b4dec50096d753f7e01629b541ca66820cb7
soundarya <soundarya.ayyappan@intainft.com>
2020-10-29 14:27:24 +0530

    Added pod-logs script

--- logs_script/pod-logs.sh
+++ logs_script/pod-logs.sh
@@ -1,69 +1,33 @@
-while getopts "c:n:o:p:O:" i
-    do
-	    case $i in
-	            c) cluster=$OPTARG ;;
-		    n) namespace=$OPTARG ;;
-		    O) org_name=$OPTARG ;;
-		    o) orderers=$(echo $OPTARG | sed 's/,/ /g') ;;
-		    p) peers=$(echo $OPTARG | sed 's/,/ /g') ;;
-	    esac
-    done
-    echo ${cluster}
+cluster=$1
+
+namespace=$2
+
+echo ${cluster}
     if [ ${cluster} == "bankema-cluster" ]
     then
         az aks get-credentials --resource-group intain-dev-rg --name ${cluster}
-        mkdir ${cluster}
-        cd ${cluster}
-        mkdir ${namespace}
-        cd ${namespace}
-        mkdir peers
-        cd peers
-        if [ -z "$peers" ]
-        then
-	        echo "no peers available in ${namespace}"
-        else
-	        for element in ${peers[@]}
-	        do
-		        kubectl -n ${namespace} logs pod/peer0-0 --all-containers=true > peer-${namespace}.log
-	        done
-        fi
-        cd .. && mkdir orderers
-        cd orderers
-        if [ -z "$orderers" ]
-        then
-	        echo "no orderers available in ${namespace}"
-        else
-	        for element in ${orderers[@]}
-	        do
-		        kubectl -n ${namespace} logs pod/orderer1-0 --all-containers=true > orderer-${namespace}.log
-	        done
-        fi
+        declare -a pod_array
+        pod_array=($(kubectl get pods -n ${namespace} | grep 2 | awk '{print $1}'))
+        array_length=${#pod_array[@]}
+        mkdir ${cluster} && cd ${cluster}
+        mkdir ${namespace} && cd ${namespace}
+        echo "Total no. of pods in ${namespace}: ${array_length}"
+        for (( c=0; c<${array_length}; c++ ))
+        do  
+          kubectl -n ${namespace} logs pod/${pod_array[c]} --all-containers=true > ${pod_array[c]}.log
+          echo "Collected logs of ${pod_array[c]}"
+        done
     else
         az aks get-credentials --resource-group intain-dev-rg --name ${cluster}
-        mkdir ${cluster}
-        cd ${cluster}
-        mkdir ${namespace}
-        cd ${namespace}
-        mkdir peers
-        cd peers
-        if [ -z "$peers" ]
-        then
-	        echo "no peers available in ${namespace}"
-        else
-	        for element in ${peers[@]}
-	        do
-		        kubectl -n ${namespace} logs deployment/peer-${element}${org_name}-hlf-peer --all-containers=true > peer-${element}${org_name}-hlf-peer.log
-	        done
-        fi
-        cd .. && mkdir orderers
-        cd orderers
-        if [ -z "$orderers" ]
-        then
-	        echo "no orderers available in ${namespace}"
-        else
-	        for element in ${orderers[@]}
-	        do
-		        kubectl -n ${namespace} logs deployment/orderer-${element}${org_name}-hlf-ord --all-containers=true > orderer-${element}${org_name}.log
-	        done
-        fi
+        declare -a pod_array
+        pod_array=($(kubectl get pods -n ${namespace} | grep 1 | awk '{print $1}'))
+        array_length=${#pod_array[@]}
+        mkdir ${cluster} && cd ${cluster}
+        mkdir ${namespace} && cd ${namespace}
+        echo "Total no. of pods in ${namespace}: ${array_length}"
+        for (( c=0; c<${array_length}; c++ ))
+        do  
+          kubectl -n ${namespace} logs pod/${pod_array[c]} --all-containers=true > ${pod_array[c]}.log
+          echo "Collected logs of ${pod_array[c]}"
+        done
     fi
